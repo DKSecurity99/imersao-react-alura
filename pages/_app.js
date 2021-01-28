60a2dd967bc739d3d53d9801d33982d51bef9970
DKSecurity99 <dksorteio@gmail.com>
2021-01-28 15:38:21 -0300

    feat: added db.theme in theme global

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+
+import db from '../db.json';
+import GlobalStyle from '../styles/globals';
+
+const theme = db.theme;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+      <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
