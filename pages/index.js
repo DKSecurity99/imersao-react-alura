0d1b31db09fe9ab26296588b4c1f811ecae08b8f
DKSecurity99 <dksorteio@gmail.com>
2021-01-28 15:39:41 -0300

    feat: completed the creation of the home page for class 1, the immersion react alura

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+import db from '../db.json';
+
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import GithubCorner from '../src/components/GitHubCorner';
+import Footer from '../src/components/Footer';
+
+export const QuizContainer = styled.article`
+  width: 100%;
+  max-width: 35rem;
+  padding-top: 4rem;
+  margin: auto 3rem;
+
+  @media screen and (max-width: 500px) {
+    padding-top: 2rem;
+  }
+`;
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage={db.bgPrimary}>
+      <QuizContainer>
+        
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+      </QuizContainer>
+      <GithubCorner projectUrl="https://github.com/DKSecurity99/imersao-react-alura"/>
+    </QuizBackground>
+  )
+}
