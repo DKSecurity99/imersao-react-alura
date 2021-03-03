7ccbabfe4fd77c5f030ce403607f9e85a334dee9
edmilson-dk <droidvidaboa@gmail.com>
2021-03-03 09:47:58 -0300

    feat: created quiz page

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,8 +1,55 @@
+import db from '../db.json';
+
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
+
+function LoadingScreen() {
+  return (
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
+      <Widget.Content>
+        [Desafio loading]
+      </Widget.Content>
+    </Widget>
+  )
+}
+
 function QuizPage({}) {
   return (
-    <div>
-      <h1>Quiz page</h1>
-    </div>
+    <QuizBackground>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h3>Pergunta 1 de {`${db.questions.length}`}</h3>
+          </Widget.Header>
+          
+          <img 
+            alt="Descrição"
+            style={{
+              width: '100%',
+              height: '150px',
+              objectFit: 'cover'
+            }}
+            src={db.bgSecond}
+          />
+          <Widget.Content>
+            <h2>Titúlo</h2>
+            <p>Descrição</p>
+
+            <Button 
+              disabled={false}
+              label="Confimar"
+              type="button"
+            />
+          </Widget.Content>
+        </Widget>
+        <LoadingScreen />
+      </QuizContainer>
+    </QuizBackground>
   )
 }
 
